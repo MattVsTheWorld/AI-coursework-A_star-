aedfe1a733df9a068091a50f20686f020f8be504
Matteo <matteo.marcuzzo.it@gmail.com>
2017-02-07 10:41:03 +0000

    First commit

---
+++ B00273607_AI_part1/graph.h
@@ -0,0 +1,85 @@
+#ifndef GRAPH
+#define GRAPH
+#include <list>
+#include <vector>
+#include <iostream>
+using namespace std;
+template <typename vertex>
+// rather use integers
+
+class graph {
+protected:
+	void addEdge(int, int) = 0;
+	int countNodes() const = 0;
+	//given a vertex return list of vertex's adjacent to it
+	virtual std::list<vertex> adjacent(int) = 0;
+	std::vector<vertex> nodeList;
+private:
+	int numNodes;
+
+};
+
+// https://en.wikipedia.org/wiki/DOT_(graph_description_language)
+// http://stackoverflow.com/questions/28313901/how-to-parse-a-dot-file-in-python
+// http://www.graphviz.org/content/how-use-graphviz-library-c-project
+// http://www.cplusplus.com/forum/lounge/65474/
+// http://www.cplusplus.com/forum/beginner/65178/
+// http://www.cplusplus.com/doc/tutorial/files/
+// http://www.cplusplus.com/reference/fstream/ofstream/
+// http://softwareengineering.stackexchange.com/questions/148313/what-is-the-most-space-efficient-way-to-implement-a-graph-data-structure
+// http://www.redblobgames.com/pathfinding/a-star/implementation.html
+// http://www.cplusplus.com/articles/2LywvCM9/
+// http://www.cplusplus.com/forum/beginner/118340/
+// http://www.cplusplus.com/forum/general/41244/
+// http://www.sanfoundry.com/cpp-program-implement-adjacency-matrix/
+// http://stackoverflow.com/questions/2893129/what-does-mean-in-c
+class AdjacencyMatrix
+{
+private:
+	int n;
+	int **adj;
+	bool *visited;
+public:
+	AdjacencyMatrix(int n)
+	{
+		this->n = n;
+		visited = new bool[n];
+		adj = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			adj[i] = new int[n];
+			for (int j = 0; j < n; j++)
+			{
+				adj[i][j] = 0;
+			}
+		}
+	}
+	/*
+	* Adding Edge to Graph
+	*/
+	void add_edge(int origin, int destin)
+	{
+		if (origin > n || destin > n || origin < 0 || destin < 0)
+		{
+			cout << "Invalid edge!\n";
+		}
+		else
+		{
+			adj[origin - 1][destin - 1] = 1;
+		}
+	}
+	/*
+	* Print the graph
+	*/
+	void display()
+	{
+		int i, j;
+		for (i = 0;i < n;i++)
+		{
+			for (j = 0; j < n; j++)
+				cout << adj[i][j] << "  ";
+			cout << endl;
+		}
+	}
+};
+#endif
---
+++ B00273607_AI_part1/main.cpp
@@ -0,0 +1,55 @@
+//
+//=======================================================================
+// B00273607
+// AI coursework - part 1
+// A* pathfinding
+//=======================================================================
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "graph.h"
+
+using namespace std;
+//
+//#include <unordered_map>
+//using namespace std;
+//
+//// http://www.redblobgames.com/pathfinding/a-star/implementation.html
+//template<typename L>
+//struct SimpleGraph {
+//	typedef L Location;
+//	typedef typename vector<Location>::iterator iterator;
+//	unordered_map<Location, vector<Location>> edges;
+//
+//	inline const vector<Location> neighbors(Location id) {
+//		return edges[id];
+//	}
+//};
+
+// E.G.
+// { 0 , { 3 , 7 } };
+//
+
+int main(int argc, char **argv) {
+	ifstream dotfile;
+	//dotfile.open("graph.dot");
+	string line;
+	if (dotfile.is_open())
+	{
+	if (dotfile.is_open())
+		cout << "File opened.\n";
+		while (getline(dotfile, line))
+		{
+			cout << line << "\n";
+		}
+		dotfile.close();
+	}
+	if (!dotfile.is_open())
+		cout << "File not open.\n";
+
+
+	return 0;
+}
