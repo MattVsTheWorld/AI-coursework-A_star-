25cad75db7a1f49cbecbcbd71ed0234605bdf1e7
Matteo <matteo.marcuzzo.it@gmail.com>
2017-02-20 21:13:32 +0000

    Added pseudocode and ref

--- B00273607_AI_part1/A_star.cpp
+++ B00273607_AI_part1/A_star.cpp
@@ -5,6 +5,30 @@ A_star::A_star(AdjacencyList *_adjList) {
 	iterations = 0;
 }
 
+// Pseudocode by Sebastian Lague
+// https://github.com/SebLague/Pathfinding/blob/master/Episode%2001%20-%20pseudocode/Pseudocode#L3
+//OPEN //the set of nodes to be evaluated
+//CLOSED //the set of nodes already evaluated
+//add the start node to OPEN
+//
+//loop
+//current = node in OPEN with the lowest f_cost
+//remove current from OPEN
+//add current to CLOSED
+//
+//if current is the target node //path has been found
+//return
+//
+//foreach neighbour of the current node
+//if neighbour is not traversable or neighbour is in CLOSED
+//skip to the next neighbour
+//
+//if new path to neighbour is shorter OR neighbour is not in OPEN
+//set f_cost of neighbour
+//set parent of neighbour to current
+//if neighbour is not in OPEN
+//add neighbour to OPEN
+
 // TODO: Convert to heap? prio queue?
 void A_star::algorithm(vertex* start, vertex* end) {
 	openSet.push_back(start);
