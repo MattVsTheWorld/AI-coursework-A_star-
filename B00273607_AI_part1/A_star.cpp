39afe03ce57946468df511480f5a13e66fc39243
Matteo Marcuzzo <matteo.marcuzzo_@hotmail.it>
2017-02-22 13:43:38 +0000

    confus

--- B00273607_AI_part1/A_star.cpp
+++ B00273607_AI_part1/A_star.cpp
@@ -76,7 +76,7 @@ void A_star::algorithm(vertex* start, vertex* end) {
 				continue;
 			} // else cout << "Not found in closed set" << endl;
 			int newMovCostToVert = currentVert->getgCost() + (static_cast<neighbour>(neighbourIterator)).second; //weight
-
+			// Uncertain
 			if (newMovCostToVert < ((static_cast<neighbour>(neighbourIterator)).first)->getgCost() || (find(openSet.begin(), openSet.end(), (static_cast<neighbour>(neighbourIterator)).first) == openSet.end()))
 			{
 				(static_cast<neighbour>(neighbourIterator)).first->setgCost(newMovCostToVert);
